3a0f98e2f0ee869a23b0ee5aeb0cd2e7969b4a29
Alex Stewart <astewart1510@gmail.com>
2021-11-09 15:52:56 +0200

    updated files

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import django
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'algorand_dashboard.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'algorand_dashboard.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
